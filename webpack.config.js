08d762871d25499436918154e100a35af1514f0f
Isabella <isabellaafry@gmail.com>
2019-05-24 11:39:41 +0100

    fix deploy please

--- webpack.config.js
+++ webpack.config.js
@@ -10,7 +10,7 @@ module.exports = (env) => {
     const currentPath = path.join(__dirname);
 
     const basePath = `${currentPath}/.env`;
-    const envPath = `${basePath}.${env.ENVIRONMENT}`;
+    const envPath = env ? `${basePath}` : `${basePath}.${env.ENVIRONMENT}`;
 
     // Check if the file exists, otherwise fall back to the production .env
     const finalPath = fs.existsSync(envPath) ? envPath : basePath;
